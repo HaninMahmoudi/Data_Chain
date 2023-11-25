88be2a7441d22ce4e8f4dcb0c308f22eb86f6356
Mariam Ghamgui <mariam.ghamgui11@gmail.com>
2023-11-25 01:27:55 +0100

    update-readme

--- Users.sol
+++ Users.sol
@@ -7,11 +7,13 @@ contract Users {
 
     event UserRegistered(address indexed user, string pseudonym, string dataTopic);
     event DataPosted(address indexed user, string data, uint256 feesEarned, string dataTopic);
+    event KYCVerified(address indexed user);
 
     struct User {
         string pseudonym;
         bool isRegistered;
         string dataTopic;
+        bool isKYCVerified;
     }
 
     struct Data {
@@ -26,6 +28,8 @@ contract Users {
     mapping(uint256 => Data) public data;
     mapping(string => uint256[]) public topicToData;
 
+    bool public kycEnabled;  // Flag to indicate whether KYC is enabled
+
     modifier userNotRegistered() {
         require(!users[msg.sender].isRegistered, "User is already registered");
         _;
@@ -36,10 +40,22 @@ contract Users {
         _;
     }
 
+    modifier kycEnabledOnly() {
+        require(kycEnabled, "KYC is not yet enabled");
+        _;
+    }
+
     constructor() {}
 
+    function enableKYC() external {
+        // Implement KYC enabling logic, such as owner-only access
+        // ...
+
+        kycEnabled = true;
+    }
+
     function registerUser(string memory _pseudonym, string memory _dataTopic) external userNotRegistered {
-        users[msg.sender] = User(_pseudonym, true, _dataTopic);
+        users[msg.sender] = User(_pseudonym, true, _dataTopic, false);
         emit UserRegistered(msg.sender, _pseudonym, _dataTopic);
         userCount++;
     }
@@ -57,14 +73,14 @@ contract Users {
         dataCount++;
     }
 
+    function verifyKYC() external kycEnabledOnly userRegistered {
+        users[msg.sender].isKYCVerified = true;
+        emit KYCVerified(msg.sender);
+    }
+
     function calculateFees() internal pure returns (uint256) {
         // This is a simple example; you can implement a more complex logic for fee calculation.
         return 1 ether; // Fixed fee for simplicity; adjust as needed.
     }
-
-    // New function to set the status of data for sale
-    function setIsForSale(uint256 _dataId, bool _isForSale) external {
-        require(data[_dataId].user == msg.sender, "You can only update your own data");
-        data[_dataId].isForSale = _isForSale;
-    }
 }
+
